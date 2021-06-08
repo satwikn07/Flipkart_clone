f742f985a54b51e2bae8c4ece20620ea30c961e6
minakshidast <minakshid606@gmail.com>
2021-06-08 17:05:31 +0530

    carousel changes

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -3,39 +3,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
  
 function CarouselBox() {
-//     var x = document.getElementsByClassName("mySlides");
-//    console.log(x)
-//     var slideIndex = 0;
-// var timeOutId;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//     showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//     // Stop the timeout from triggering.
-//     clearTimeout(timeOutId);
-
-//     var i;
-//     var x = document.getElementsByClassName("mySlides");
-//     if (n==undefined){n = ++slideIndex;}
-//     if (n > x.length) {slideIndex = 1;} 
-//     if (n < 1) {slideIndex = x.length;}
-//     for (let i = 0; i < x.length; i++) {
-//          x[i].style.display = "none"; 
-//     }
-//     x[slideIndex+1].style.display = "block"; 
-//     //Schedule a new timeout.
-//     timeOutId = setTimeout(showDivs, 8000); // Change image every 8 seconds
-// }
+ 
 
         return (
-            <Carousel  showThumbs={false}  infiniteLoop={true}  useKeyboardArrows={true} autoPlay={true} >
+            <div style={{marginTop:"130px"}}> 
+            <Carousel   showThumbs={false}  infiniteLoop={true}  useKeyboardArrows={true} autoPlay={true} >
                  
 
                  
-                <div     className="mySlides">
+                <div  className="mySlides">
                 <img    style={{width:"1366px", height:"267px"}} src="https://rukminim1.flixcart.com/flap/844/140/image/7795510bfaf2adcf.jpg" alt="HMAK1X4NM5ZI"/>
                 </div>
                 <div  className="mySlides">
@@ -49,6 +25,7 @@ function CarouselBox() {
                 </div>
                
             </Carousel>
+            </div>
         );
     }
 
