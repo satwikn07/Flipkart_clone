c6ef190ddf5a72111928a63bd655af113c97ea1b
minakshidast <minakshid606@gmail.com>
2021-06-09 20:28:14 +0530

    changes

---
+++ src/Components/AboveFooter.jsx
@@ -0,0 +1,11 @@
+
+
+function AboveFooter(){
+    return(
+        <div>
+            <div>Top Stories: Brand Directory</div>
+            <div>MOST SEARCHED FOR ON FLIPKART:TV & Appliances Super Saver Days SaleTV & Appliances Shop From Home DaysGrand Home Appliances SaleFlipkart Super Cooling DaysBig Screen TelevisionsTV & Large Appliances Big Savings Day SaleAkshaya Tritiya OffersEd A Mamma ClothingBooksFlipkart Electronics: TV & Home Appliances SaleFlipkart Cooling DaysAC Cooling Days 2021Refrigerator Cooling Days 2021Grand Kitchen & Home Appliances SaleStreak SunglassesGold CoinsFlipkart PlusFlipkart Super CoinFlipkart COVID-19 InsuranceFlipkart Help CentreFlipkart Track OrdersFlipkart Manage OrdersFlipkart Return OrdersContact LensesFlipkart Gift Cards StoreSuper Fan ShowSareesFlipkart Axis Bank Credit CardFlipkart VideosFlipkart Pay LaterRedmi 8Honor 20iOPPO K1Flipkart Axis Bank Credit CardEarringsMens WatchesSilver CoinsJewelleryWatchesGold CoinMangalsutraSunglassesMi SunglassesBackpacksRay Ban Sunglasses</div>
+        </div>
+    )
+}
+export {AboveFooter}
