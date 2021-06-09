4a7f56be0f27622381a863fce7c5d826f598e80b
afzal <afzalpolite@gmail.com>
2021-06-10 00:38:23 +0530

    thirddiv update

--- src/Components/ThirdHeaderDiv.jsx
+++ src/Components/ThirdHeaderDiv.jsx
@@ -5,20 +5,20 @@ import '@brainhubeu/react-carousel/lib/style.css';
 import styles from "../CSS/ThirdDiv.module.css";
 
 function ThirdHeader(){
-    let [hr,setHr] = useState(2);
-    let [min,setMin] = useState(2);
-    let [sec,setSec] = useState(2);
+    let [hr,setHr] = useState(23);
+    let [min,setMin] = useState(59);
+    let [sec,setSec] = useState(59);
     var x;
     if(sec==0 && min==0 && hr==0){
         clearInterval(x);
     }
     else if(sec==-1){
         setMin(pre=>pre-1);
-        setSec(2);
+        setSec(59);
     }
     else if(min==-1){
         setHr(pre=>pre-1);
-        setMin(2);
+        setMin(59);
     }
 
     useEffect(()=>{
@@ -30,17 +30,6 @@ function ThirdHeader(){
             clearInterval(x)
         }
     },[])
-    // function clockStart() {
-
-        // if(min==0){
-        //     setHr(pre=>pre-1);
-        //     setMin(59);
-        // }
-        // if(hr==0){
-        //     console.log("expired");
-        // }
-
-//   }
 
     return <div style={{backgroundColor:"white"}}>
 
@@ -66,7 +55,7 @@ function ThirdHeader(){
             <div className={styles.card}>
             <div style={{height:"150px"}}><img width="100%" height="80%" src="https://rukminim1.flixcart.com/image/150/150/jmjhifk0/sanitary-pad-pantyliner/s/z/6/ultra-clean-plus-xl-44-sanitary-pad-whisper-original-imaf9ezuqgkpayhc.jpeg?q=70" alt="pic" /></div>
             <div style={{textAlign:"center"}}>
-            <h6>Healthcare</h6>
+            <strong>Healthcare</strong>
             <p>Extra 5% off</p>
             <p>masks, body pain,relief </p>
             </div>
@@ -75,7 +64,7 @@ function ThirdHeader(){
             <div className={styles.card} >
            <div style={{height:"150px"}}><img width="100%" height="80%" src="https://rukminim1.flixcart.com/image/150/150/kevpwnk0/vehicle-pull-along/k/5/b/35300-2319a-adventure-force-original-imafvgmmuzhjvaej.jpeg?q=70" alt="pic" /></div>
             <div style={{textAlign:"center"}}>
-            <h6>Healthcare</h6>
+            <strong>Healthcare</strong>
             <p>Extra 5% off</p>
             <p>masks, body pain,relief </p>
             </div>
@@ -84,7 +73,7 @@ function ThirdHeader(){
             <div className={styles.card} >
            <div style={{height:"150px"}}><img width="100%" height="80%" src="https://rukminim1.flixcart.com/image/150/150/k65d18w0pkrrdj/shoe/h/m/f/6-xxzbnnwkov-kraasa-original-imafnbtjwgzbgwzn.jpeg?q=70" alt="pic" /></div>
             <div style={{textAlign:"center"}}>
-            <h6>Healthcare</h6>
+            <strong>Healthcare</strong>
             <p>Extra 5% off</p>
             <p>masks, body pain,relief </p>
             </div>
@@ -93,7 +82,7 @@ function ThirdHeader(){
             <div className={styles.card} >
             <div style={{height:"150px"}}><img width="100%" height="80%" src="https://rukminim1.flixcart.com/image/150/150/kikluvk0-0/diaper/v/i/k/m-wonder-pullups-pant-style-premium-diaper-with-wetness-original-imafybngsw38qjrh.jpeg?q=70" alt="pic" /></div>
             <div style={{textAlign:"center"}}>
-            <h6>Healthcare</h6>
+            <strong>Healthcare</strong>
             <p>Extra 5% off</p>
             <p>masks, body pain,relief </p>
             </div>
@@ -102,7 +91,7 @@ function ThirdHeader(){
             <div className={styles.card} >
             <div style={{height:"150px"}}><img width="100%" height="80%" src="https://rukminim1.flixcart.com/image/150/150/jsw3yq80/container/b/3/c/magna-12-pcs-container-set-blue-cello-original-imafed34nwvrmshy.jpeg?q=70" alt="pic" /></div>
             <div style={{textAlign:"center"}}>
-            <h6>Healthcare</h6>
+            <strong>Healthcare</strong>
             <p>Extra 5% off</p>
             <p>masks, body pain,relief </p>
             </div>
@@ -111,7 +100,7 @@ function ThirdHeader(){
             <div className={styles.card} >
             <div style={{height:"150px"}}><img width="100%" height="80%" src="https://rukminim1.flixcart.com/image/150/150/kehfi4w0/sticker/y/h/r/wallpaper-happy-winter-trees-and-frames-home-diy-self-adhesive-original-imafv5n5qfzwyt8k.jpeg?q=70" alt="pic" /></div>
             <div style={{textAlign:"center"}}>
-            <h6>Healthcare</h6>
+            <strong>Healthcare</strong>
             <p>Extra 5% off</p>
             <p>masks, body pain,relief </p>
             </div>
