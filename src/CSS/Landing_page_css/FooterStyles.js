0aa4a97094bd380e2fdd7c40047a3f4d8565d14e
afzal <afzalpolite@gmail.com>
2021-06-22 23:45:09 +0530

    update

--- src/CSS/Landing_page_css/FooterStyles.js
+++ src/CSS/Landing_page_css/FooterStyles.js
@@ -23,7 +23,7 @@ export const Container = styled.div`
 	display: flex;
 	flex-direction: column;
  
-	 width: 100%;
+    width: 100%;
 	margin: 0 auto;
  
 `
--- src/Components/Checkout.jsx/Checkout_Login_Page.jsx
+++ src/Components/Checkout.jsx/Checkout_Login_Page.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect,useContext } from 'react';
+import React, { useState,useContext } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import styles from "./Checkout_login.module.css";
